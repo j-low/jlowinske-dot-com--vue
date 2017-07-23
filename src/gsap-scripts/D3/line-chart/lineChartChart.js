d7c8e2606e5da11c8fba131b87c8680f14d9ecdb
Joe Lowinske <lowi0008@gmail.com>
2017-07-23 09:34:27 -0500

    add fade and shift to line chart nodes

--- src/gsap-scripts/D3/line-chart/lineChartChart.js
+++ src/gsap-scripts/D3/line-chart/lineChartChart.js
@@ -12,19 +12,32 @@ function enterTimeline() {
   const content = $('.content');
   const container = $('.line-chart-container');
   const chart = $('.line-chart-container svg');
+  const nodes = $('.data-node');
   const line = $('.line');
   var mainTl = new TimelineLite();
+  var nodesTl = new TimelineLite();
+  var nodeTweens = [];
   var textTl = text.enterTimeline();
 
   var contentPadding = TweenLite.fromTo(content, 0.3, { paddingBottom: 10 }, { paddingBottom: 0 });
   var containerFade = TweenLite.fromTo(container, 0.3, { opacity: 0 }, { opacity: 1 });
   var chartFade = TweenLite.fromTo(chart, durationEnter, { opacity: 0 }, { opacity: 1 });
+
+  _.each(nodes, function(n) {
+    let posNeg = Math.random() < 0.5 ? -1 : 1;
+    let shift = posNeg * Math.floor(Math.random() * 10);
+    let fadeAndShift = TweenLite.fromTo(n, 0.3, { opacity: 0, y: shift }, { opacity: 1, y: 0 });
+    nodeTweens.push(fadeAndShift);
+  });
+
+  nodesTl.add(nodeTweens);
+
   var drawLine = TweenLite.fromTo(line, 1.25, { drawSVG: '0%' }, { drawSVG: '100%' });
 
   mainTl
     .add([contentPadding, containerFade, textTl])
     .add([chartFade])
-    .add(drawLine);
+    .add([nodesTl, drawLine]);
 
   return mainTl;
 }
